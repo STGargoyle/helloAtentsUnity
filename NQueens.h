f696a5e0880f7e4a5edcafb0f037df67e68731aa
STGargoyle <124038523+STGargoyle@users.noreply.github.com>
2023-05-03 09:04:14 +0900

    230503 알고리즘
    강의에 앞서 코딩해올 부분

---
+++ NQueens.h
@@ -0,0 +1,6 @@
+#pragma once
+#define NQUEENS_H
+
+void PrintSolution(int Columns[], int NumberOfQueens);
+int IsThreatened(int Columns[], int NewRow);
+void FindSolutionForQueen(int Columns[], int Row, int NumberOfQueens, int* SolutionCount);
